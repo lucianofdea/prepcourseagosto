f3d39e92d00d90cfb6cc04ff04e1b4781ee942bd
Lucianofdea <originalbady@hotmail.com>
2023-08-02 19:40:53 -0300

    mi primer commit

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,13 +5,13 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = 'Hola';
 
 // Crea una variable de tipo number.
-const nuevoNumero = null;
+const nuevoNumero = 5;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = null;
+const nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - null === 3;
